908f0ecd1d2118a593ff0ecb19f27506afc01a68
eidsid <eidsiddiedis@gmail.com>
2023-05-25 07:59:41 +0200

    add mongodb url

--- DB/db.js
+++ DB/db.js
@@ -1,13 +1,17 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const connectDB = () => {
-    return mongoose.connect(process.env.dbURL, {
+  return mongoose
+    .connect(
+      "mongodb+srv://eid:123@learnnodeexpressdb.76ext.mongodb.net/URIS?retryWrites=true&w=majority",
+      {
         useNewUrlParser: true,
         useUnifiedTopology: true,
+      }
+    )
+    .then(() => {
+      console.log("connected to db");
+    });
+};
 
-    }).then(() => {
-        console.log('connected to db')
-    })
-}
-
-module.exports = connectDB
+module.exports = connectDB;
--- app.js
+++ app.js
@@ -3,33 +3,33 @@ const app = express();
 const path = require("path");
 const method_override = require("method-override");
 const DB = require("./DB/db");
-const flash = require('express-flash')
-const session = require('express-session')
-const passport = require('passport')
+const flash = require("express-flash");
+const session = require("express-session");
+const passport = require("passport");
 
 // middlewares
 
-// call passport Config 
-const passportConfig = require('./Config/passportConfig');
-passportConfig(passport)
+// call passport Config
+const passportConfig = require("./Config/passportConfig");
+passportConfig(passport);
 
-
-// express session 
-app.use(session({
-    secret: 'secret',
+// express session
+app.use(
+  session({
+    secret: "secret",
     resave: false,
     saveUninitialized: true,
-}))
-app.use(passport.initialize())
-app.use(passport.session())
-
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 // connect flash
-app.use(flash())
+app.use(flash());
 
 // call flash message config
-const flashConfig = require('./Config/flashConfig')
-app.use(flashConfig)
+const flashConfig = require("./Config/flashConfig");
+app.use(flashConfig);
 
 require("dotenv").config();
 
@@ -40,9 +40,9 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(
-    express.urlencoded({
-        extended: false,
-    })
+  express.urlencoded({
+    extended: false,
+  })
 );
 
 app.use(method_override("_method"));
@@ -53,12 +53,11 @@ app.use("/users", users);
 const url = require("./router/url");
 app.use("/", url);
 
-
 const port = process.env.PORT || 9000;
-const start = async() => {
-    await DB();
-    app.listen(port, () => {
-        console.log(`you are listen in port ${port}`);
-    });
+const start = async () => {
+  await DB();
+  app.listen(port, () => {
+    console.log(`you are listen in port ${port}`);
+  });
 };
 start();
